5819dab7f9fdd19ead18efcf08283d6b9f259c36
mariosantos-05 <marioaugustov.s@gmail.com>
2025-12-06 21:54:20 -0300

    Update routes behavior

--- app/controllers/admins/forms_controller.rb
+++ app/controllers/admins/forms_controller.rb
@@ -18,22 +18,43 @@ class Admins::FormsController < ApplicationController
   end
 
   def create
-    template_id = params[:template_id]
-    turma_ids   = params[:turma_ids] || []
+  template_id = params[:template_id]
+  turma_ids   = params[:turma_ids] || []
 
-    if template_id.blank? || turma_ids.empty?
-      redirect_to new_admin_form_path, alert: "Selecione um template e pelo menos uma turma."
-      return
+  if template_id.blank? || turma_ids.empty?
+    flash.now[:alert] = "Selecione um template e pelo menos uma turma."
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: [
+          turbo_stream.remove("modal"), 
+          turbo_stream.replace(
+            "flash_messages",
+            partial: "shared/flash"
+          )
+        ]
+      end
+
+      format.html do
+        redirect_to admin_management_path, alert: "Selecione um template e pelo menos uma turma."
+      end
     end
 
-    turma_ids.each do |tid|
-      Form.create!(
-        template_id: template_id,
-        turma_id: tid,
-        is_active: true
-      )
-    end
-
-    redirect_to admin_management_path, notice: "Formulários criados com sucesso!"
+    return
   end
+
+  # SUCCESS CASE
+  turma_ids.each do |tid|
+    Form.create!(
+      template_id: template_id,
+      turma_id: tid,
+      is_active: true
+    )
+  end
+
+  redirect_to admin_management_path, notice: "Formulários criados com sucesso!"
+end
+
+
+
 end
