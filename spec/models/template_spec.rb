af345f9f6b254d4f7b1a49464bbd9f01880d926b
Luís Filipe <135170965+luisfilipe3@users.noreply.github.com>
2025-12-07 17:43:05 -0300

    Update template_spec.rb
    testes de unidade

--- spec/models/template_spec.rb
+++ spec/models/template_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Template, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Testes de unidade
+
+  context "Validações" do
+    it "é inválido sem título (Cenário Triste)" do
+      template = Template.new(titulo: nil)
+      template.valid?
+      expect(template.errors[:titulo]).to include("O campo Título é obrigatório")
+    end
+
+    it "é inválido com caracteres especiais no título (Cenário Triste)" do
+      template = Template.new(titulo: "Titulo@Invalido")
+      template.valid?
+      expect(template.errors[:titulo]).to include("Formato de título inválido")
+    end
+
+    it "é inválido sem questões (Cenário Triste)" do
+      template = Template.new(titulo: "Valido")
+      # Simulando ausência de questões
+      template.questions = [] 
+      template.valid?
+      expect(template.errors[:base]).to include("O template deve conter pelo menos uma questão")
+    end
+  end
 end
