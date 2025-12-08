f26d4d63e00ff8ede04b03fd0fac5936aea56dc3
Luís Filipe <135170965+luisfilipe3@users.noreply.github.com>
2025-12-08 18:33:20 -0300

    Update templates_controller.rb

--- app/controllers/admins/templates_controller.rb
+++ app/controllers/admins/templates_controller.rb
@@ -1,31 +1,17 @@
 class Admins::TemplatesController < ApplicationController
-
-  def index
-    @templates = Template.all
-
-    # Cenário: Visualizar lista vazia
-    if @templates.empty?
-      flash.now[:notice] = "Nenhum template foi criado"
-    end
-  end
-
+  
   def new
     @template = Template.new
+    @template.questions.build
   end
 
   def create
     @template = Template.new(template_params)
-
-    # Atribui o usuário logado
-    @template.criado_por = current_user 
-
-    # Simula questões vindas do form
-    @template.questions = params[:template][:questions] 
+    @template.criado_por = current_user
 
     if @template.save
-      redirect_to templates_path, notice: "Template criado com sucesso"
+      redirect_to admins_templates_path, notice: "Template criado com sucesso"
     else
-      # Cenários tristes: Título vazio ou Sem questões
       flash.now[:alert] = @template.errors.full_messages.join(", ")
       render :new, status: :unprocessable_entity
     end
@@ -38,28 +24,30 @@ class Admins::TemplatesController < ApplicationController
   def update
     @template = Template.find(params[:id])
 
-    # Simula questões
-    @template.questions = ["mock_question"] 
-
-
     if @template.update(template_params)
-      redirect_to templates_path, notice: "Template atualizado com sucesso"
+      redirect_to admins_templates_path, notice: "Template atualizado com sucesso"
     else
-      # Cenário Triste: Caracteres inválidos
-      flash.now[:alert] = @template.errors.full_messages.first
+      flash.now[:alert] = @template.errors.full_messages.join(", ")
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
     @template = Template.find(params[:id])
-    @template.destroy
-    redirect_to templates_path, notice: "Template removido com sucesso"
+    if @template.destroy
+      redirect_to admins_templates_path, notice: "Template removido com sucesso"
+    else
+      redirect_to admins_templates_path, alert: "Não foi possível remover o template."
+    end
   end
 
   private
 
   def template_params
-    params.require(:template).permit(:titulo, :target_audience)
+    params.require(:template).permit(
+      :titulo, 
+      :target_audience,
+      questions_attributes: [:id, :text, :question_type, :options, :_destroy]
+    )
   end
 end
