1ddc236ccf4cf0032585c6729bd11e7524c76c0e
mariosantos-05 <marioaugustov.s@gmail.com>
2025-12-07 21:30:51 -0300

    Comment fix

--- app/controllers/avaliacoes_controller.rb
+++ app/controllers/avaliacoes_controller.rb
@@ -26,13 +26,11 @@ class AvaliacoesController < ApplicationController
   def responder
     @form = Form.includes(:template, :turma).find(params[:form_id])
 
-    # Verifica se o usuário participa da turma
     unless current_user.vinculos.exists?(turma_id: @form.turma_id)
       redirect_to avaliacoes_path, alert: "Você não tem acesso a esse formulário."
       return
     end
 
-    # 🚨 Impede que responda mais de 1 vez
     if Resposta.exists?(form_id: @form.id, usuario_id: current_user.id)
       redirect_to avaliacoes_path, alert: "Você já respondeu este formulário."
       return
@@ -42,25 +40,23 @@ class AvaliacoesController < ApplicationController
   def enviar_resposta
     form = Form.find(params[:form_id])
 
-    # Verifica permissão
+
     unless current_user.vinculos.exists?(turma_id: form.turma_id)
       redirect_to avaliacoes_path, alert: "Você não tem acesso a esse formulário."
       return
     end
 
-    # 🚨 Impede que o aluno envie novamente (segurança)
+
     if Resposta.exists?(form_id: form.id, usuario_id: current_user.id)
       redirect_to avaliacoes_path, alert: "Você já respondeu este formulário."
       return
     end
 
-    # 👉 Aqui criamos o registro dizendo que ESTE aluno respondeu
     Resposta.create!(
       form_id: form.id,
       usuario_id: current_user.id
     )
 
-    # ❗ Nada de destruir o form — cada aluno responde o mesmo form
     redirect_to avaliacoes_path, notice: "Formulário enviado com sucesso!"
   end
 
