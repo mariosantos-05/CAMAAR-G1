6c92991c742b376cd62783e625a88841022a5bce
mariosantos-05 <marioaugustov.s@gmail.com>
2025-12-08 12:30:39 -0300

    Visualização de turmas para teste

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,30 +2,37 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   allow_browser versions: :modern
 
-
-  #This whole section is to mock user authentication for development/testing purposes.
   def current_user
-    fake_role = ENV["FAKE_ROLE"] || "admin"   #default to admin
+    fake_role = ENV["FAKE_ROLE"] || "admin"
 
-    case fake_role
-    when "admin"
-      Usuario.find_or_create_by(profile: "Admin") do |u|
-        u.nome = "Fake Admin"
-        u.email = "admin@test.com"
-        u.matricula = "000"
-        u.status = true
-        u.departamento_id = 1
+    user =
+      case fake_role
+      when "admin"
+        Usuario.find_or_create_by(profile: "Admin") do |u|
+          u.nome = "Fake Admin"
+          u.email = "admin@test.com"
+          u.matricula = "000"
+          u.status = true
+          u.departamento_id = 1
+        end
+      when "user"
+        Usuario.find_or_create_by(profile: "User") do |u|
+          u.nome = "Fake User"
+          u.email = "user@test.com"
+          u.matricula = "111"
+          u.status = true
+          u.departamento_id = 1
+        end
+      else
+        return nil
       end
-    when "user"
-      Usuario.find_or_create_by(profile: "User") do |u|
-        u.nome = "Fake User"
-        u.email = "user@test.com"
-        u.matricula = "111"
-        u.status = true
-        u.departamento_id = 1
+
+    Turma.find_each do |turma|
+      Vinculo.find_or_create_by(usuario_id: user.id, turma_id: turma.id) do |v|
+        v.papel_turma = 0
       end
-    else
-      nil
     end
+
+    user
   end
 end
